ff1c910ff064204eb369fe7eb89496e81913d593
Роман Кузин <roman.kuzin@accenture.com>
2021-04-01 13:10:22 +0300

    add new files

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Git");
-        //1
-        //2
-        //3
+        String statements = "rta-statements-state-employee";
+
+        if(statements.contains("rta-statements")){
+            System.out.println("contains!");
+        }
     }
 }

4b42d035866ec05ba7d447014b66bd4ad7e3d5bf
Роман Кузин <roman.kuzin@accenture.com>
2021-06-03 10:43:11 +0300

    add "Hello Natasha!"

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello Git");
-        String statements = "rta-statements-state-employee";
-
-        if(statements.contains("rta-statements")){
-            System.out.println("contains!");
-        }
     }
 }
